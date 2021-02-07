240c4da7e547cae8cf5ef8b5590ae6ac98c4980b
Yan Nagler <yannagler@Yans-MacBook-Pro.local>
2021-02-07 16:43:51 +0200

    run.sh - update

--- run.sh
+++ run.sh
@@ -4,8 +4,13 @@
 set -e
 
 
-py_dir="/Users/yannagler/Desktop/dev-ops-course/py"
-pkgs_dir="$py_dir/venv/lib/python3.9/site-packages/"
+usr = 'yannagler'
+crs_dir = "/Users/$usr/Desktop/dev-ops-course"
+py_dir = "$crs_dir/py"
+
+py = "python3.9"
+pkgs_dir="$py_dir/venv/lib/$py/site-packages"
+
 
 export PYTHONPATH="$pkgs_dir:$PYTHONPATH"
 echo $PYTHONPATH
@@ -14,13 +19,13 @@ echo
 
 python3.9 backend_testing_db.py
 
-nohup python3.9 rest_app.py &
-nohup python3.9 web_app.py &
+nohup $py rest_app.py &
+nohup $py web_app.py &
 sleep 1
-python3.9 backend_testing.py
-python3.9 frontend_testing.py
-python3.9 combined_testing.py
-python3.9 clean_environment.py
+$py backend_testing.py
+$py frontend_testing.py
+$py combined_testing.py
+$py clean_environment.py
 
 echo "Done"
 
