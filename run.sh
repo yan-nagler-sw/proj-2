6464d60ea022ecc3cbb2f46e5d18e0b7142553df
Yan Nagler <yannagler@Yans-MacBook-Pro.local>
2021-02-07 13:59:43 +0200

    run.sh - update

--- run.sh
+++ run.sh
@@ -4,17 +4,13 @@
 set -e
 
 
-ws_dir="/Users/yannagler/Desktop/dev-ops-course/py"
-pkgs_dir="$ws_dir/venv/lib/python3.9/site-packages/"
-proj_dir="$ws_dir/proj-1"
+py_dir="/Users/yannagler/Desktop/dev-ops-course/py"
+pkgs_dir="$py_dir/venv/lib/python3.9/site-packages/"
 
-PYTHONPATH=""
 export PYTHONPATH="$pkgs_dir:$PYTHONPATH"
 echo $PYTHONPATH
 echo
 
-
-cd $proj_dir
 
 python3.9 backend_testing_db.py
 
